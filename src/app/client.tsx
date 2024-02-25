e15345e13a426a6231b3b759791ea27747c5c385
Obafemi Ogunmokun <o@blackboxai.tech>
2024-02-25 13:53:42 +0100

    Infinite Scrolling

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -1,23 +1,59 @@
 "use client";
 
 import CardItem from "@/components/CardItem";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 type Props = {
   cards: Card[];
 };
 
-const HomeClientPage = ({ cards }: Props) => {
+const HomeClientPage: React.FC<Props> = ({ cards }: Props) => {
   const [search, setSearch] = useState("");
+  const [filteredCards, setFilteredCards] = useState<Card[]>(cards);
+  const [loadedCards, setLoadedCards] = useState<Card[]>([]);
+  const [hasMore, setHasMore] = useState(true);
 
-  const filteredList = cards.filter((card) =>
-    card.name.toLowerCase().includes(search.toLowerCase())
-  );
+  useEffect(() => {
+    // Apply initial search filter
+    setFilteredCards(
+      cards.filter((card) =>
+        card.name.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+    // Fetch initial 20 cards
+    setLoadedCards(filteredCards.slice(0, 20));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [search]);
+
+  const fetchMoreCards = async () => {
+    if (!hasMore) return;
+
+    const nextIndex = loadedCards.length;
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    const newCards = cards.slice(nextIndex, nextIndex + 20); // Simulate fetching next 20 cards
+
+    // Apply search filter to newly fetched cards
+    const filteredNewCards = newCards.filter((card) =>
+      card.name.toLowerCase().includes(search.toLowerCase())
+    );
+
+    setLoadedCards([...loadedCards, ...filteredNewCards]);
+
+    // Check if there are more cards to load
+    setHasMore(filteredNewCards.length === 20);
+  };
 
   return (
-    <div className="py-10 px-20">
-      <div className="flex items-center justify-end gap-4 mb-8">
-        <p>{search ? filteredList.length : cards.length}</p>
+    <div className="py-6 sm:py-10 px-10 sm:px-20">
+      <div className="flex items-center justify-center sm:justify-end gap-4 mb-8">
+        <p className="text-xs text-gray-400">
+          {search
+            ? loadedCards.length
+            : filteredCards.length > 0
+            ? filteredCards.length
+            : cards.length}
+        </p>
         <input
           placeholder="search name"
           value={search}
@@ -25,28 +61,35 @@ const HomeClientPage = ({ cards }: Props) => {
           className="px-2 py-1 border border-gray-300 rounded text-sm"
         />
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-6 gap-8">
-        {search ? (
-          filteredList.length > 0 ? (
-            filteredList.map((card) => (
-              <div key={card.id}>
-                <CardItem card={card} />
-              </div>
-            ))
+      <InfiniteScroll
+        dataLength={loadedCards.length}
+        next={fetchMoreCards}
+        hasMore={hasMore}
+        loader={
+          search ? (
+            ""
           ) : (
-            <div className="text-center text-gray-500">No results found.</div>
+            <div className="text-center text-gray-500">Loading...</div>
           )
-        ) : cards.length > 0 ? (
-          cards.map((card) => (
+        }
+        endMessage={
+          loadedCards.length === 0 && filteredCards.length === 0 ? (
+            <div className="text-center text-gray-500">No cards available.</div>
+          ) : (
+            <div className="text-center text-gray-500">
+              No more cards to load.
+            </div>
+          )
+        }
+      >
+        <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-6 gap-8">
+          {loadedCards.map((card) => (
             <div key={card.id}>
               <CardItem card={card} />
             </div>
-          ))
-        ) : (
-          // Display a message when there are no cards at all
-          <div className="text-center text-gray-500">No cards available.</div>
-        )}
-      </div>
+          ))}
+        </div>
+      </InfiniteScroll>
     </div>
   );
 };
